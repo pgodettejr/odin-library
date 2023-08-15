7491b7ace400bf82d9220f21c05eb60d79d6c6e4
pgodettejr <paul.godette@gmail.com>
2023-08-15 13:30:07 -0400

    Add Book constructor

---
+++ odin-library.js
@@ -0,0 +1,21 @@
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = function() {
+      if (read === 'yes') {
+        return 'read';
+      } else {
+        return 'not read';
+      }
+    }
+    this.info = function(){
+      console.log(title, author, pages, read)
+    }
+  }
+
+function addBookToLibrary() {
+    // add code here
+}
