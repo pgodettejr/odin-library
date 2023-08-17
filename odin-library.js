6691120f0a2979be73623ae5a74dbe4e8813dc5a
pgodettejr <paul.godette@gmail.com>
2023-08-17 14:07:58 -0400

    Add descriptions for current functions

--- odin-library.js
+++ odin-library.js
@@ -1,28 +1,31 @@
 const myLibrary = [];
 
+// Constructor for making "Book" objects & reporting "Book" info
 function Book(title, author, pages, read) {
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.read = function() {
-      if (read === 'yes') {
-        return 'read';
-      } else {
-        return 'not read';
-      }
-    }
-    this.info = function(){
-      console.log(title, author, pages, read)
+  this.title = title
+  this.author = author
+  this.pages = pages
+  this.read = function() {
+    if (read === 'yes') {
+      return 'read';
+    } else {
+      return 'not read';
     }
   }
-
-function addBookToLibrary() {
-    let enterBook = prompt("Please add a book title").toLowerCase();
-    myLibrary.push(enterBook);
+  this.info = function(){
+    console.log(title, author, pages, read)
+  }
 }
 
+// Stores new Book objects into myLibrary array via user input
+function addBookToLibrary() {
+  let enterBook = prompt("Please add a book title").toLowerCase();
+  myLibrary.push(enterBook);
+}
+
+// Loops through myLibrary array & displays each Book on the webpage (via table or "card")
 function bookList() {
-    for (let i = 0; i < myLibrary.length; i++) {
+  for (let i = 0; i < myLibrary.length; i++) {
         
-    }
+  }
 }
